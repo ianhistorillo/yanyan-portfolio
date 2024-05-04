1ef74dde4d2c8deb479a4e422a834064b4d89d16
Christian Historillo <ianworks@Christians-MacBook-Pro.local>
2024-05-05 01:31:23 +0800

    Adding updates on my new portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import "./styles/components/_navigation.scss";
 import "./styles/components/_pages.scss";
 import Navigation from "./components/Navigation/Navigation";
 import Philippines from "./components/Icons/Philippines";
+import LinkedIn from "./components/Icons/LinkedIn";
+import Github from "./components/Icons/Github";
 
 export default function Home() {
   return (
@@ -12,22 +14,34 @@ export default function Home() {
         <div className="header">
           <div className="greetings">
             {" "}
-            Greetings from Philippines! <Philippines />{" "}
+            Mabuhay! <Philippines />
           </div>
-          <span> Hi there, I'm</span>
-          <h1> Christian Historillo </h1>
+          <div className="content">
+            I'm <span className="bold">Christian Historillo</span> a software
+            engineer. I thrive on turning ideas into digital reality - with a
+            blend of creativity and technical prowess, I craft seamless web
+            experiences that captivate and engage users.
+          </div>
+          <div className="get-in-touch">
+            <span className="contact-me-text"> Contact me - </span>
+            <span className="email-add">
+              {" "}
+              <a href="mailto:ian.webworks@gmail.com">ian.webworks@gmail.com</a>
+            </span>
+            <div className="social-profile">
+              <a
+                href="https://www.linkedin.com/in/christian-historillo-4069501a1/"
+                target="_blank"
+              >
+                <LinkedIn />
+              </a>
+              <a href="https://github.com/ianhistorillo" target="_blank">
+                <Github />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="content">
-          <p className="paragraph-text">
-            {" "}
-            As a passionate full-stack web developer, I thrive on turning ideas
-            into digital reality. With a blend of creativity and technical
-            prowess, I craft seamless web experiences that captivate and engage
-            users. From front-end aesthetics to back-end functionality, I
-            specialize in bringing projects to life with clean code and
-            intuitive design.
-          </p>
-        </div>
+
         {/* <div className="i-col50">TEST</div>
        <div className="i-col50">TEST</div>
          <div className="i-col50">TEST</div>
