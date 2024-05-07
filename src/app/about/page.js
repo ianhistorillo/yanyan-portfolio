4ccab31a5f69f51bfe28d9aacab36a0f8d3b5376
Christian Historillo <ianworks@Christians-MacBook-Pro.local>
2024-05-08 00:55:19 +0800

    Adding software about page content

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -11,13 +11,57 @@ export default function Home() {
   return (
     <div className="main">
       <div className="i-col100 i-container">
-        <div className="header">
-          <div className="greetings"> About</div>
+        <div className="i-col50 i-fl">
+          <div className="header">
+            <div className="header-title"> BACKGROUND </div>
+            <div className="side-nav">
+              <div className="active">
+                <span className="line transition-all"></span>
+                <span className="text"> How it all started? </span>
+              </div>
+              <div>
+                <span className="line transition-all"></span>
+                <span className="text"> Goals </span>
+              </div>
+              <div>
+                <span className="line transition-all"></span>
+                <span className="text"> Experience </span>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* <div className="i-col50">TEST</div>
-       <div className="i-col50">TEST</div>
-         <div className="i-col50">TEST</div>
-         <div className="i-col50">TEST</div> */}
+        <div className="i-col50 i-fr">
+          <div className="content-page">
+            <p>
+              {" "}
+              When I was a kid, maybe around grade school, I was fascinated by
+              computer machines. I saw one for the very first time in the
+              computer shop in my neighborhood. I was curious about how this
+              technology worked, and because of that, I dreamed of having one.
+            </p>
+            <p>
+              But because it was expensive at that time, I couldn't afford to
+              have one. However, this did not stop me from trying it out,
+              discovering it, and eventually, it carved a path to my passion for
+              technology.
+            </p>
+            <p>
+              At first, I only enjoyed how the computer entertained me through
+              gaming, but when I first discovered Friendster - my very first
+              social media platform - my curiosity was piqued again, eventually
+              leading to the development of my passion for web development and
+              software engineering.
+            </p>
+            <p>
+              {" "}
+              From a kid who dreamed of building his own social media platform
+              to becoming a web and software developer as a career, the dream
+              became a reality, and it continues to grow. I'm still hungry for
+              new knowledge and experiences, and I still have a constant desire
+              to push my boundaries and create something new every day.
+            </p>
+          </div>
+        </div>
         <Navigation activePage="About" />
       </div>
     </div>
